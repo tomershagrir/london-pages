992f84c90ecef006cb102fc0339810f10746cbbc
Marinho Brandao <marinho@gmail.com>
2011-11-23 17:05:08 +0000

    New default templates

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,6 +1,6 @@
 import os 
 
 from london.apps.themes.registration import register_template
-register_template("page_view", mirroring="page_view.html")
+register_template("page_view", mirroring="pages/page_view.html")
 
 
