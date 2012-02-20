d0a0f50657110150ef8919e07d29929f0a69ad2a
Juan Pablo Romero <jpablo.romero@gmail.com>
2012-02-20 00:30:42 -0600

    Raise a validation error if attempting to insert a duplicated slug on a site

--- pages/forms.py
+++ pages/forms.py
@@ -1,9 +1,21 @@
 from london import forms
+from london.forms.base import ErrorList
+from pages.models import Page
 
 __author__ = 'jpablo'
 
+class PageForm(forms.ModelForm):
+    class Meta:
+        model = Page
+        exclude = ('text',)
+        readonly = ('last_update', 'text')
 
-class PageForm(forms.Form):
-    name = forms.CharField(max_length=255)
-    slug = forms.CharField(max_length=255)
-    text = forms.TextField()
+    def clean(self):
+        cleaned_data = super(PageForm, self).clean()
+        slug = cleaned_data['slug']
+        pages = cleaned_data['site']['pages']
+        if pages.filter(slug=slug).count() > 0:
+            self._errors['slug'] = ErrorList('slug',['duplicated slug'])
+            del cleaned_data['slug']
+
+        return cleaned_data
