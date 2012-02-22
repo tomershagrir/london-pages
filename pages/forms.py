bb8079dfa411a75241e1738373134a257fe7f1ee
juan pablo romero <jpablo.romero@gmail.com>
2012-02-22 09:22:54 -0600

    signal support on form creation and object saving

--- pages/forms.py
+++ pages/forms.py
@@ -1,7 +1,10 @@
 from london import forms
 from pages.models import Page
+from pages import signals
 
 __author__ = 'jpablo'
+
+
 
 class PageForm(forms.ModelForm):
     class Meta:
@@ -11,6 +14,7 @@ class PageForm(forms.ModelForm):
 
     def initialize(self):
         self.fields['keywords'].widget = forms.ListByCommasTextInput()
+        signals.page_form_initialize.send(sender=self)
 
     def clean(self):
         cleaned_data = super(PageForm, self).clean()
@@ -25,17 +29,12 @@ class PageForm(forms.ModelForm):
         if pages.count() > 0:
             raise forms.ValidationError('Another page with slug "%s" already exists for this site.'%slug, field_name='slug')
 
+        signals.page_form_clean.send(sender=self, cleaned_data=cleaned_data)
         return cleaned_data
 
-    """
-    def clean(self):
-        cleaned_data = super(PageForm, self).clean()
-        slug = cleaned_data['slug']
-        pages = cleaned_data['site']['pages']
-        if pages.filter(slug=slug).count() > 0:
-            self._errors['slug'] = ErrorList('slug',['duplicated slug'])
-            del cleaned_data['slug']
-
-        return cleaned_data
-    """
+    def save(self, commit=True, force_new=False):
+        signals.page_form_pre_save.send(sender=self, instance=self.instance)
+        obj = super(PageForm, self).save(commit, force_new)
+        signals.page_form_post_save.send(sender=self, instance=obj)
+        return obj
 
---
+++ pages/signals.py
@@ -0,0 +1,7 @@
+from london.dispatch import Signal
+
+page_form_initialize = Signal()
+page_form_pre_save = Signal()
+page_form_post_save = Signal()
+page_form_clean = Signal()
+
