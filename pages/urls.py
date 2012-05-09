1216895630f62a4214d8f503b32390208006b3e6
Thiago Avelino <thiagoavelinoster@gmail.com>
2012-05-09 01:11:12 -0300

    if set home create pattern url

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,13 @@
+# coding: utf-8
 
 from london.urls.defining import patterns
+from pages.models import Page
 
 url_patterns = patterns('pages.views',
         (r'^(?P<slug>[\w-]+)/$', 'view', {}, "page_view"),
 )
+
+if Page.query().filter(is_home=True):
+    url_patterns += patterns('pages.views',
+        (r'^$', 'view_home', {}, "page_view_home"),
+    )
