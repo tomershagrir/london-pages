13f1033e3ec602f5fd936a03929c587f42b243e0
Thiago Avelino <thiagoavelinoster@gmail.com>
2012-05-09 13:19:58 -0300

    Create rule is home redirect page to home page

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@
 
 from london.shortcuts import get_object_or_404
 from london.templates import render_to_response
+from london.apps.ajax.tags import redirect_to
 from pages.models import Page
 
 def _return_view(request, slug, template):
@@ -11,6 +12,12 @@ def _return_view(request, slug, template):
         {'page': page})
 
 def view(request, slug, template="page_view"):
+    try:
+        if slug == Page.query().filter(is_home=True).get()['slug']:
+            return redirect_to(request, '/')
+    except:
+        pass
+
     return _return_view(request, slug, template)
 
 def view_home(request, template="page_view"):
