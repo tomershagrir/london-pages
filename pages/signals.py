7173889c90a47317409a29ad13fd528efe4fde8f
bisibuka <bisibuka@gmail.com>
2012-07-02 14:03:13 +0300

    added collection support

--- pages/signals.py
+++ pages/signals.py
@@ -1,11 +1,14 @@
 from london.dispatch import Signal
 from images import add_image_field_to_sender_form
 from london.apps.seo import add_meta_info_fields_to_sender_form, save_meta_info_from_sender_form
+from london.apps.collections import add_collections_to_sender_form, save_collections_from_sender_form
 
 page_form_initialize = Signal()
 page_form_initialize.connect(add_meta_info_fields_to_sender_form)
 page_form_initialize.connect(add_image_field_to_sender_form)
+page_form_initialize.connect(add_collections_to_sender_form)
 page_form_pre_save = Signal()
 page_form_post_save = Signal()
 page_form_post_save.connect(save_meta_info_from_sender_form)
+page_form_post_save.connect(save_collections_from_sender_form)
 page_form_clean = Signal()
